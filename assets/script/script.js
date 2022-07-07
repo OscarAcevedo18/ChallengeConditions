0bc007f3c7e6c1b7676c06295f692b4e73589e36
Oscar Acevedo <oacevedovilches@gmail.com>
2022-07-07 10:37:13 -0400

    image with functional border is added

---
+++ assets/script/script.js
@@ -0,0 +1,8 @@
+document.querySelector('#button').onclick = function border () {
+    if(button.style.border === 'none'){
+        button.style.border = 'solid 5px yellow'  
+        }
+    else{
+        button.style.border = 'none';
+        }
+    }
