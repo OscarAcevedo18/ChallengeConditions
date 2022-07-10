a84e33e1f75ff522e089a4a0b8bea85476ef09ea
Oscar Acevedo <oacevedovilches@gmail.com>
2022-07-10 12:07:55 -0400

    modified javascript

--- assets/script/script.js
+++ assets/script/script.js
@@ -12,11 +12,16 @@ if (button.style.borderStyle != "solid") {
 let btn = document.querySelector('#btn-one');
 let msj = document.querySelector ('#msj');
     btn.addEventListener("click", (e) => {
-quantity = Number(document.querySelector("#input-one").value) +
-        Number (document.querySelector("#input-two").value) +
-        Number (document.querySelector("#input-three").value);
+let input_one = document.querySelector("#input-one").value 
+let input_two = document.querySelector("#input-two").value
+let input_three = document.querySelector("#input-three").value
+let quantity = Number(input_one) + Number(input_two) + Number(input_three)
 
-if (quantity == 0) {
+if (isNaN(input_one) || isNaN(input_two) || isNaN(input_three)) {
+msj.innerHTML="//Ingresar Solo Numeros//"
+msj.setAttribute('style', 'background-color: orange')
+}
+else if(quantity == 0) {
 msj.innerHTML="=== No Seleccionaste Stickers ===";
 msj.setAttribute('style', 'background-color: blue')
 } else if (quantity >10){
@@ -33,13 +38,13 @@ msj.setAttribute('style', 'background-color: green',)
 let btnTwo = document.querySelector('#btn-two');
 let psw = document.querySelector ('#psw');
     btnTwo.addEventListener("click", () => {
-passw = Number(document.querySelector("#num-one").value + 
+let passw = Number(document.querySelector("#num-one").value + 
 document.querySelector("#num-two").value +
 document.querySelector("#num-three").value);
-if (passw == "911") {
+if (passw === 911) {
 psw.innerHTML="Password 1 Correcto";
 psw.setAttribute('style', 'background-color: green');
-} else if (passw == "714") {
+} else if (passw === 714) {
 psw.innerHTML="Password 2 Correcto";
 psw.setAttribute('style', 'background-color: green');
 } else {
